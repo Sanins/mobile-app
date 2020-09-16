c50318d45c06c851c06bc464c873ea06c25bbd2d
Robert Sanins <rob.sanins@lovehoney.co.uk>
2020-09-16 22:45:31 +0100

    add google fit  to app

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeScreen } from './homeScreen/HomeScreen';
+import GoogleFit, { Scopes } from 'react-native-google-fit';
 
 export default function App() {
+    useEffect(() => {
+      const options = {
+        scopes: [
+          Scopes.FITNESS_ACTIVITY_READ_WRITE,
+          Scopes.FITNESS_BODY_READ_WRITE,
+        ],
+      }
+    GoogleFit.authorize(options)
+      .then((res) => {
+        console.log('authorized >>>', res)
+      })
+      .catch((err) => {
+        console.log('err >>> ', err)
+      });
+  });
+
   return (
     <NavigationContainer>
       <HomeScreen />
--- app/homeScreen/HomeScreen.tsx
+++ app/homeScreen/HomeScreen.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import { Button, View, Text, Image, ScrollView, TextInput } from 'react-native';
+import GoogleFit from 'react-native-google-fit';
+import moment from 'moment';
 
 export const HomeScreen = () => {
+  const apples = () => {
+  let dayStart = moment().startOf('day').toISOString(); //ISO Time String
+  let dayEnd = moment().endOf('day').toISOString();//ISO Time String
+
+  const options = {
+      startDate: dayStart,  // required ISO8601Timestamp
+      endDate: dayEnd     // required ISO8601Timestamp
+  };
+
+  GoogleFit.getDailyStepCountSamples(options, (err, res) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Daily steps >>>", res);
+  })
+  }
+
   return (
     <ScrollView>
-    <Text>Some text</Text>
+    <Text>count</Text>
+    <Button
+      title="Right button"
+      onPress={() => apples()}
+    />
     <View>
         <Text>Some more text</Text>
         <Image
